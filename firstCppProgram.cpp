c0ba957d91475d8c5dde34567f1142d42e665108
singhAbhi1234 <144318926+singhAbhi1234@users.noreply.github.com>
2023-09-22 05:20:56 -0600

    Add files via upload

---
+++ firstCppProgram.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+main(){
+	
+	 char name[]="abhi";
+	int age;
+	cout<<" Welcome to first c++ program"<<endl;
+	cout<<"enter your age";
+	cin>>age;
+	cout<<"your age is: "<<age<<endl<<"your name is:"<<name;
+}
