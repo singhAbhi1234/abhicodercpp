2c48f09a4c033da4172d56f255141605efd4bfbc
singhAbhi1234 <144318926+singhAbhi1234@users.noreply.github.com>
2023-09-22 09:06:20 -0600

    Add files via upload

---
+++ addTwoNumbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+main(){
+	double a;
+	double b;
+	
+	double sum;
+	
+	cout<<"Enter first number and second number"<<endl;
+	cin>>a>>b;
+	sum=a+b;
+	cout<<"the sum is:"<<sum<<endl;
+}
